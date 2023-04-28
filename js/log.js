c60280f1d75b5dca9329634f8fae15ba2425dfd7
Aashi Jaulim <aashi.jaulim@gmail.com>
2023-04-28 22:30:36 +0400

    connected to db

--- js/log.js
+++ js/log.js
@@ -1,17 +1,19 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-  
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
 
 // Get the modal
 var modal = document.getElementById('id01');
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
+
+// Get the modal
+var modal2 = document.getElementById('id02');
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
+
