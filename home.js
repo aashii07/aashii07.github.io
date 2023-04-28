0dcf222fd8bc81f81ad6d33766c33380fada967a
Aashi Jaulim <aashi.jaulim@gmail.com>
2023-04-28 12:43:21 +0400

    form, login

---
+++ home.js
@@ -0,0 +1,17 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
