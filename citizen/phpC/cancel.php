615c17f4b0fa9b3a9028088b4e1f396aea83e2a9
Aashi Jaulim <aashi.jaulim@gmail.com>
2023-05-24 22:58:34 +0400

    mail3

--- citizen/phpC/cancel.php
+++ citizen/phpC/cancel.php
@@ -1,6 +1,25 @@
 <?php
     // Start the session
     session_start();
+
+    // Include the Composer autoloader
+    require 'PHPMailer-master/vendor/autoload.php';
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    // Create a new PHPMailer instance
+    $mail = new PHPMailer(true);
+
+    // Set up SMTP configuration
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';  // Specify your SMTP server
+    $mail->Port = 587;  // Specify the SMTP port
+    // Enable SMTP encryption
+    $mail->SMTPSecure = 'tls'; // or 'ssl' for SSL encryption
+    $mail->SMTPAuth = true;
+    $mail->Username = 'aashi.jaulim@gmail.com';  // Your SMTP username
+    $mail->Password = 'hhjqbxsjjwrqbpee';  // Your SMTP password
    
     //DB connection
     $db=new mysqli('localhost', 'root', '!AAshi4477', 'fyp');
@@ -11,9 +30,44 @@
     else{
         $id = $_GET['id'];
 
+        $email=$_SESSION['email'];
+        $q="SELECT * FROM public WHERE email='$email'";
+        $r=mysqli_query($db, $q);
+        $row=mysqli_fetch_assoc($r);
+        $fname=$row['firstname'];
+
+        $q="SELECT * FROM incident WHERE id='$id'";
+        $r=mysqli_query($db, $q);
+        $row=mysqli_fetch_assoc($r);
+        $des=$row['description'];
+
         
         $caller="DELETE FROM incident WHERE id='$id'";
         $result=mysqli_query($db, $caller);
+
+        // Set up email parameters
+        $mail->setFrom('aashi.jaulim@gmail.com', 'SAMU IMS');
+        $mail->addAddress($email, 'Public');
+        $mail->Subject = 'Cancellation of Incident Report';
+        $mail->Body = 'Dear '.$fname.',
+
+You have requested to cancel the incident report with the following description: '.$des.'
+
+Your request has been received and processed accordingly. 
+Thank you for your collaboration.
+
+Best regards,
+SAMU IMS Team';
+        
+        try {
+        // Send the email
+                $mail->send();
+                echo 'Email sent successfully.';
+        } catch (Exception $e) {
+                echo 'An error occurred. Email not sent.';
+                echo 'Error: ' . $mail->ErrorInfo;
+        }
+
 
 
         header("location: home.php");
